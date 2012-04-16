c20206643561d64b5d2edb51c289f2f3bcc03465
Krzysztof Menżyk <krzysztof@menzyk.net>
2012-04-16 12:10:52 +0200

    Build bootstrap started from a separate PHP process Issue #28

---
+++ Resources/bin/build_bootstrap.php
@@ -0,0 +1,26 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * This file is part of the Symfony Standard Edition.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$argv = $_SERVER['argv'];
+
+// allow the base path to be passed as the first argument, or default
+if (isset($argv[1])) {
+    $appDir = $argv[1];
+} else {
+    if (!$appDir = realpath(__DIR__.'/../../../../../../../../app')) {
+        exit('Looks like you don\'t have a standard layout.');
+    }
+}
+
+require_once $appDir.'/autoload.php';
+
+\Sensio\Bundle\DistributionBundle\Composer\ScriptHandler::doBuildBootstrap($appDir);
