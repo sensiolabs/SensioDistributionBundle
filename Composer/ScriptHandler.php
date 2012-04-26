fd69fb3ca525ab3e91065c8a114dd76e2b42549f
François Pluchino <francois.pluchino@gourmeal.net>
2012-04-26 20:48:31 +0300

    Use of the escapeshellarg method

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -109,10 +109,10 @@ class ScriptHandler
     {
         $phpFinder = new PhpExecutableFinder;
         $php = escapeshellarg($phpFinder->find());
-        $cmd = __DIR__.'/../Resources/bin/build_bootstrap.php';
+        $cmd = escapeshellarg(__DIR__.'/../Resources/bin/build_bootstrap.php');
         $appDir = escapeshellarg($appDir);
 
-        $process = new Process($php.' "'.$cmd.'" '.$appDir);
+        $process = new Process($php.' '.$cmd.' '.$appDir);
         $process->run(function ($type, $buffer) { echo $buffer; });
     }
 
