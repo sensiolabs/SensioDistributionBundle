186893175d31a569d551b0c3318ecd419e2d2166
vurtupesz <php4225>
2012-07-23 23:51:50 +0200

    Test intl configuration

--- Resources/skeleton/app/SymfonyRequirements.php
+++ Resources/skeleton/app/SymfonyRequirements.php
@@ -505,6 +505,12 @@ class SymfonyRequirements extends RequirementCollection
         );
 
         $this->addRecommendation(
+            !(extension_loaded('intl') && null === new IntlDateFormatter()),
+            'intl extension should be correctly configured',
+            'If your project uses intl, you must fix it. This problem is typical on PHP 5.3.X x64 WIN build'
+        );
+
+        $this->addRecommendation(
             class_exists('DomDocument'),
             'PHP-XML module should be installed',
             'Install and enable the <strong>PHP-XML</strong> module.'
