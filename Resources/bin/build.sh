1d2bab55a00bf22f7fdbf13116f9f39ccfdec157
Fabien Potencier <fabien.potencier@gmail.com>
2012-08-29 11:45:59 +0200

    reworked the build.sh script to create archives that works fine with composer (no removed .git in vendors)

--- Resources/bin/build.sh
+++ Resources/bin/build.sh
@@ -7,87 +7,86 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
-DIR=`php -r "echo realpath(dirname(\\$_SERVER['argv'][0]));"`
-cd $DIR
-VERSION=`grep ' VERSION ' vendor/symfony/symfony/src/Symfony/Component/HttpKernel/Kernel.php | sed -E "s/.*'(.+)'.*/\1/g"`
-
-if [ ! -d "$DIR/build" ]; then
-    mkdir -p $DIR/build
+if [ ! $1 ]; then
+    echo "\033[37;41mYou must pass the build dir as an absolute path\033[0m"
+    exit 1
 fi
 
-# Without vendors
-rm -rf /tmp/Symfony
-mkdir /tmp/Symfony
-cp -r app /tmp/Symfony/
-cp -r src /tmp/Symfony/
-cp -r web /tmp/Symfony/
-cp README.md /tmp/Symfony/
-cp LICENSE /tmp/Symfony/
-cp composer.json /tmp/Symfony/
-cp composer.lock /tmp/Symfony/
-cd /tmp/Symfony
-sudo rm -rf app/cache/* app/logs/* .git*
-chmod 777 app/cache app/logs
+DIR=$1
+CURRENT=`php -r "echo realpath(dirname(\\$_SERVER['argv'][0]));"`
 
-# DS_Store cleanup
-find . -name .DS_Store | xargs rm -rf -
+if [[ ! "$DIR" = /* ]]; then
+     DIR="$CURRENT/$DIR"
+fi
 
-cd ..
+if [ ! -d $DIR ]; then
+    echo "\033[37;41mThe build dir does not exist\033[0m"
+    exit 1
+fi
+
 # avoid the creation of ._* files
 export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
-tar zcpf $DIR/build/Symfony_Standard_$VERSION.tgz Symfony
-sudo rm -f $DIR/build/Symfony_Standard_$VERSION.zip
-zip -rq $DIR/build/Symfony_Standard_$VERSION.zip Symfony
+
+# Temp dir
+rm -rf /tmp/Symfony
+mkdir /tmp/Symfony
+
+# Clone
+cd /tmp/Symfony
+git clone https://github.com/symfony/symfony-standard.git .
+
+# alpha as a minimum stability as we don't want clones
+sed -i '' -e's/"minimum-stability"\: ".*"/"minimum-stability": "alpha"/' composer.json
+composer.phar update
+
+# cleanup
+sudo rm -rf app/cache/* app/logs/* .git*
+chmod 777 app/cache app/logs
+find . -name .DS_Store | xargs rm -rf -
+
+VERSION=`grep ' VERSION ' vendor/symfony/symfony/src/Symfony/Component/HttpKernel/Kernel.php | sed -E "s/.*'(.+)'.*/\1/g"`
 
 # With vendors
-cd $DIR
-rm -rf /tmp/vendor
-mkdir /tmp/vendor
-TARGET=/tmp/vendor
+cd /tmp/Symfony
+TARGET=/tmp/Symfony/vendor
 
-if [ ! -d "$DIR/vendor" ]; then
-    echo "The master vendor directory does not exist"
-    exit
-fi
-
-cp -r $DIR/vendor/* $TARGET/
-
-# Doctrine ORM
+# Doctrine
 cd $TARGET/doctrine/orm && rm -rf UPGRADE* build* bin tests tools lib/vendor
-
-# Doctrine DBAL
 cd $TARGET/doctrine/dbal && rm -rf bin build* tests lib/vendor
-
-# Doctrine Common
 cd $TARGET/doctrine/common && rm -rf build* tests lib/vendor
+cd $TARGET/doctrine/doctrine-bundle && rm -rf Doctrine/Bundle/DoctrineBundle/Tests Doctrine/Bundle/DoctrineBundle/Resources/doc
+
+# JMS
+cd $TARGET/jms/metadata && rm -rf README.rst phpunit.xml* tests
+cd $TARGET/jms/cg && rm -rf README.rst phpunit.xml* tests
+cd $TARGET/jms/aop-bundle/JMS/AopBundle && rm -rf phpunit.xml* Tests Resources/doc
+cd $TARGET/jms/di-extra-bundle/JMS/DiExtraBundle && rm -rf phpunit.xml* Tests Resources/doc
+cd $TARGET/jms/security-extra-bundle/JMS/SecurityExtraBundle && rm -rf phpunit.xml* Tests Resources/doc
+
+# kriswallsmith
+cd $TARGET/kriswallsmith/assetic && rm -rf CHANGELOG* phpunit.xml* tests docs
+
+# Monolog
+cd $TARGET/monolog/monolog && rm -rf README.markdown phpunit.xml* tests
+
+# Sensio
+cd $TARGET/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle && rm -rf phpunit.xml* Tests CHANGELOG* Resources/doc
+cd $TARGET/sensio/framework-extra-bundle/Sensio/Bundle/FrameworkExtraBundle && rm -rf phpunit.xml* Tests CHANGELOG* Resources/doc
+cd $TARGET/sensio/generator-bundle/Sensio/Bundle/GeneratorBundle && rm -rf phpunit.xml* Tests CHANGELOG* Resources/doc
 
 # Swiftmailer
 cd $TARGET/swiftmailer/swiftmailer && rm -rf CHANGES README* build* docs notes test-suite tests create_pear_package.php package*
 
 # Symfony
 cd $TARGET/symfony/symfony && rm -rf README.md phpunit.xml* tests *.sh vendor
+cd $TARGET/symfony/assetic-bundle/Symfony/Bundle/AsseticBundle && rm -rf Tests Resources/doc
+cd $TARGET/symfony/swiftmailer-bundle/Symfony/Bundle/SwiftmailerBundle && rm -rf Tests Resources/doc
+cd $TARGET/symfony/monolog-bundle/Symfony/Bundle/MonologBundle && rm -rf Tests Resources/doc
 
 # Twig
 cd $TARGET/twig/twig && rm -rf AUTHORS CHANGELOG README.markdown bin doc package.xml.tpl phpunit.xml* test
-
-# Twig Extensions
 cd $TARGET/twig/extensions && rm -rf README doc phpunit.xml* test
-
-# Monolog
-cd $TARGET/monolog/monolog && rm -rf README.markdown phpunit.xml* tests
-
-# Sensio
-cd $TARGET/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/ && rm -rf phpunit.xml* Tests CHANGELOG*
-cd $TARGET/sensio/framework-extra-bundle/Sensio/Bundle/FrameworkExtraBundle/ && rm -rf phpunit.xml* Tests CHANGELOG*
-cd $TARGET/sensio/generator-bundle/Sensio/Bundle/GeneratorBundle/ && rm -rf phpunit.xml* Tests CHANGELOG*
-
-# JMS
-cd $TARGET/jms/metadata && rm -rf README.rst phpunit.xml* tests
-cd $TARGET/jms/cg && rm -rf README.rst phpunit.xml* tests
-cd $TARGET/jms/aop-bundle/JMS/AopBundle && rm -rf phpunit.xml* Tests
-cd $TARGET/jms/di-extra-bundle/JMS/DiExtraBundle && rm -rf phpunit.xml* Tests
-cd $TARGET/jms/security-extra-bundle/JMS/SecurityExtraBundle/ && rm -rf phpunit.xml* Tests
 
 # cleanup
 find $TARGET -name .git | xargs rm -rf -
@@ -95,8 +94,14 @@ find $TARGET -name .gitignore | xargs rm -rf -
 find $TARGET -name .gitmodules | xargs rm -rf -
 find $TARGET -name .svn | xargs rm -rf -
 
-cd /tmp/
-mv /tmp/vendor /tmp/Symfony/
-tar zcpf $DIR/build/Symfony_Standard_Vendors_$VERSION.tgz Symfony
-sudo rm -f $DIR/build/Symfony_Standard_Vendors_$VERSION.zip
-zip -rq $DIR/build/Symfony_Standard_Vendors_$VERSION.zip Symfony
+cd /tmp
+tar zcpf $DIR/Symfony_Standard_Vendors_$VERSION.tgz Symfony
+sudo rm -f $DIR/Symfony_Standard_Vendors_$VERSION.zip
+zip -rq $DIR/Symfony_Standard_Vendors_$VERSION.zip Symfony
+
+# Without vendors
+cd /tmp
+rm -rf Symfony/vendor
+tar zcpf $DIR/Symfony_Standard_$VERSION.tgz Symfony
+sudo rm -f $DIR/Symfony_Standard_$VERSION.zip
+zip -rq $DIR/Symfony_Standard_$VERSION.zip Symfony
