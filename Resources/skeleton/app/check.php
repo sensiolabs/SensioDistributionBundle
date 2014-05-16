704f7b756df053473ebd2dc6dd11011eb8149d64
kathryn.greer <kathryn.greer@sensiolabs.com>
2014-05-16 14:26:57 +0200

    Improve check php

--- Resources/skeleton/app/check.php
+++ Resources/skeleton/app/check.php
@@ -4,59 +4,125 @@ require_once dirname(__FILE__).'/SymfonyRequirements.php';
 
 $symfonyRequirements = new SymfonyRequirements();
 
+$okMessage = '[OK] Your system is ready to execute Symfony2 projects';
+$errorMessage = '[ERROR] Your system is not ready to execute Symfony2 projects';
+
+$lineSize = strlen($errorMessage) + 2;
+
+echo PHP_EOL;
+echo 'Symfony2 Requirements Checker'.PHP_EOL;
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~';
+
 $iniPath = $symfonyRequirements->getPhpIniConfigPath();
 
-echo "********************************\n";
-echo "*                              *\n";
-echo "*  Symfony requirements check  *\n";
-echo "*                              *\n";
-echo "********************************\n\n";
+echo_title('Looking for the INI configuration file used by PHP:');
+echo $iniPath ? $iniPath : 'WARNING: No configuration file (php.ini) used by PHP!';
 
-echo $iniPath ? sprintf("* Configuration file used by PHP: %s\n\n", $iniPath) : "* WARNING: No configuration file (php.ini) used by PHP!\n\n";
-
-echo "** ATTENTION **\n";
-echo "*  The PHP CLI can use a different php.ini file\n";
-echo "*  than the one used with your web server.\n";
-if ('\\' == DIRECTORY_SEPARATOR) {
-    echo "*  (especially on the Windows platform)\n";
-}
-echo "*  To be on the safe side, please also launch the requirements check\n";
-echo "*  from your web server using the web/config.php script.\n";
-
-echo_title('Mandatory requirements');
+echo_title('Checking Symfony requirements:');
 
 $checkPassed = true;
+$messages = array();
 foreach ($symfonyRequirements->getRequirements() as $req) {
     /** @var $req Requirement */
-    echo_requirement($req);
+    if ($helpText = getErrorMessage($req, $lineSize)) {
+        echo 'E';
+        $messages['error'][] = $helpText;
+    } else {
+        echo '.';
+    }
+
     if (!$req->isFulfilled()) {
         $checkPassed = false;
     }
 }
 
-echo_title('Optional recommendations');
-
 foreach ($symfonyRequirements->getRecommendations() as $req) {
-    echo_requirement($req);
+    if ($helpText = getErrorMessage($req, $lineSize)) {
+        echo 'W';
+        $messages['warning'][] = $helpText;
+    } else {
+        echo '.';
+    }
 }
+
+if (empty($messages['error'])) {
+    echo_result($okMessage, $lineSize, 'ok');
+}
+
+if (!empty($messages['error'])) {
+    echo_result($errorMessage, $lineSize, 'error');
+
+    echo PHP_EOL.'Fix the following mandatory requirements'.PHP_EOL;
+    echo str_repeat('~', $lineSize).PHP_EOL;
+
+    foreach ($messages['error'] as $helpText) {
+        echo ' * '.$helpText.PHP_EOL;
+    }
+}
+
+if (!empty($messages['warning'])) {
+    echo PHP_EOL.'Optional recommendations to improve your setup'.PHP_EOL;
+    echo str_repeat('~', $lineSize).PHP_EOL;
+
+    foreach ($messages['warning'] as $helpText) {
+        echo ' * '.$helpText.PHP_EOL;
+    }
+}
+
+echo PHP_EOL;
+echo 'Note  the command console could use a different php.ini file'.PHP_EOL;
+echo '~~~~  than the one used with your web server. To be on the'.PHP_EOL;
+echo '      safe side, please check the requirements from your web'.PHP_EOL;
+echo '      server using the web/config.php script.'.PHP_EOL;
+echo PHP_EOL;
 
 exit($checkPassed ? 0 : 1);
 
-/**
- * Prints a Requirement instance
- */
-function echo_requirement(Requirement $requirement)
+function getErrorMessage(Requirement $requirement, $lineSize)
 {
-    $result = $requirement->isFulfilled() ? 'OK' : ($requirement->isOptional() ? 'WARNING' : 'ERROR');
-    echo ' ' . str_pad($result, 9);
-    echo $requirement->getTestMessage() . "\n";
-
-    if (!$requirement->isFulfilled()) {
-        echo sprintf("          %s\n\n", $requirement->getHelpText());
+    if ($requirement->isFulfilled()) {
+        return;
     }
+
+    $errorMessage  = wordwrap($requirement->getTestMessage(), $lineSize - 3, PHP_EOL.'   ').PHP_EOL;
+    $errorMessage .= '   > '.wordwrap($requirement->getHelpText(), $lineSize - 5, PHP_EOL.'   > ').PHP_EOL;
+
+    return $errorMessage;
 }
 
 function echo_title($title)
 {
-    echo "\n** $title **\n\n";
+    echo PHP_EOL.PHP_EOL;
+    echo '> '.$title.PHP_EOL;
+    echo '  ';
+}
+
+function echo_result($message, $lineSize, $type)
+{
+    // ANSI color codes
+    $colorCodes = array(
+        "none"  => "\033[0m",
+        "ok"    => "\033[32m",
+        "error" => "\033[37;41m",
+    );
+
+    $lineStart = hasColorSupport() ? $colorCodes[$type] : '';
+    $lineEnd   = hasColorSupport() ? $colorCodes['none'].PHP_EOL : PHP_EOL;
+
+    echo PHP_EOL.PHP_EOL;
+
+    echo $lineStart.'+'.str_repeat('-', $lineSize).'+'.$lineEnd;
+    echo $lineStart.'|'.str_repeat(' ', $lineSize).'|'.$lineEnd;
+    echo $lineStart.'| '.$message.str_repeat(' ', $lineSize - strlen($message) - 2).' |'.$lineEnd;
+    echo $lineStart.'|'.str_repeat(' ', $lineSize).'|'.$lineEnd;
+    echo $lineStart.'+'.str_repeat('-', $lineSize).'+'.$lineEnd;
+}
+
+function hasColorSupport()
+{
+    if (DIRECTORY_SEPARATOR == '\\') {
+        return false !== getenv('ANSICON') || 'ON' === getenv('ConEmuANSI');
+    }
+
+    return function_exists('posix_isatty') && @posix_isatty(STDOUT);
 }
