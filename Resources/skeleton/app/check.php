52eae1c247bf8aec19a72b24ea6aea831e915ba3
kathryn.greer <kathryn.greer@sensiolabs.com>
2014-05-16 14:26:57 +0200

    [RFR] Modified the check.php skeleton to add a fancier display

--- Resources/skeleton/app/check.php
+++ Resources/skeleton/app/check.php
@@ -6,57 +6,99 @@ $symfonyRequirements = new SymfonyRequirements();
 
 $iniPath = $symfonyRequirements->getPhpIniConfigPath();
 
-echo "********************************\n";
-echo "*                              *\n";
-echo "*  Symfony requirements check  *\n";
-echo "*                              *\n";
-echo "********************************\n\n";
+$okMessage = '[OK] Your system is ready to execute Symfony2 projects!';
+$errorMessage = '[ERROR] Your system is not ready to execute Symfony2 projects!';
 
-echo $iniPath ? sprintf("* Configuration file used by PHP: %s\n\n", $iniPath) : "* WARNING: No configuration file (php.ini) used by PHP!\n\n";
+$lineSize = strlen($errorMessage);
 
-echo "** ATTENTION **\n";
-echo "*  The PHP CLI can use a different php.ini file\n";
-echo "*  than the one used with your web server.\n";
-if ('\\' == DIRECTORY_SEPARATOR) {
-    echo "*  (especially on the Windows platform)\n";
-}
-echo "*  To be on the safe side, please also launch the requirements check\n";
-echo "*  from your web server using the web/config.php script.\n";
+echo 'Symfony2 Requirements Checker'.PHP_EOL;
+echo str_repeat('=', $lineSize);
 
-echo_title('Mandatory requirements');
+echo_title('Looking for the INI configuration file used by PHP');
+
+echo $iniPath ? $iniPath : 'WARNING: No configuration file (php.ini) used by PHP!';
+
+echo_title('Checking mandatory requirements:');
 
 $checkPassed = true;
+$message = array();
 foreach ($symfonyRequirements->getRequirements() as $req) {
     /** @var $req Requirement */
-    echo_requirement($req);
+    if ($helpText = getHelpText($req)) {
+        echo 'E';
+        $message['E'][] = $helpText;
+    } else {
+        echo '.';
+    }
+
     if (!$req->isFulfilled()) {
         $checkPassed = false;
     }
 }
 
-echo_title('Optional recommendations');
+echo_title('Checking optional recommendations:');
 
 foreach ($symfonyRequirements->getRecommendations() as $req) {
-    echo_requirement($req);
+    if ($helpText = getHelpText($req)) {
+        echo 'W';
+        $message['W'][] = $helpText;
+    } else {
+        echo '.';
+    }
 }
+
+if (empty($message['E'])) {
+    echo_result($okMessage, $lineSize);
+}
+
+if (!empty($message['E'])) {
+    echo_result($errorMessage, $lineSize);
+
+    echo PHP_EOL.'Fix the following mandatory requirements'.PHP_EOL;
+    echo str_repeat('-', $lineSize).PHP_EOL;
+
+    foreach ($message['E'] as $helpText) {
+        echo '  * '.$helpText.PHP_EOL;
+    }
+}
+
+if (!empty($message['W'])) {
+    echo PHP_EOL.'(Optional) Fix the following recommendations'.PHP_EOL;
+    echo str_repeat('-', $lineSize).PHP_EOL;
+
+    foreach ($message['W'] as $helpText) {
+        echo '  * '.$helpText.PHP_EOL;
+    }
+}
+
+echo PHP_EOL.'Note  the command console could use a diffrent php.ini file'.PHP_EOL;
+echo '~~~~  than the one used with your web server. To be on the'.PHP_EOL;
+echo '      safe side, please check the requirements from your web'.PHP_EOL;
+echo '      server using the web/config.php script.'.PHP_EOL;
 
 exit($checkPassed ? 0 : 1);
 
 /**
  * Prints a Requirement instance
  */
-function echo_requirement(Requirement $requirement)
+function getHelpText(Requirement $requirement)
 {
-    $result = $requirement->isFulfilled() ? 'OK' : ($requirement->isOptional() ? 'WARNING' : 'ERROR');
-    echo ' ' . str_pad($result, 9);
-    echo $requirement->getTestMessage() . "\n";
-
-    if (!$requirement->isFulfilled()) {
-        echo sprintf("          %s\n\n", $requirement->getHelpText());
+    if ($requirement->isFulfilled()) {
+        return;
     }
+
+    return $requirement->getHelpText();
 }
 
 function echo_title($title)
 {
-    echo "\n** $title **\n\n";
+    echo PHP_EOL.PHP_EOL.'> '.$title.PHP_EOL;
+}
+
+function echo_result($message, $lineSize)
+{
+    echo PHP_EOL.PHP_EOL;
+    echo str_repeat('=', $lineSize).PHP_EOL;
+    echo $message.PHP_EOL;
+    echo str_repeat('=', $lineSize).PHP_EOL;
 }
