a4d000fc222254a3efb8fde66c26e1b3a46b29d5
Loick Piera <pyrech@gmail.com>
2014-06-05 01:27:58 +0200

    [New directory structure] Fix phpunit.xml.dist kernel's directory

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -438,8 +438,19 @@ namespace { return \$loader; }
 !bin/symfony_requirements
 /composer.phar
 EOF;
-
-        $phpunit = str_replace(array('<directory>../src/', '"bootstrap.php.cache"'), array('<directory>src/', '"'.$varDir.'/bootstrap.php.cache"'), file_get_contents($rootDir.'/phpunit.xml.dist'));
+        $phpunitKernelBefore = <<<EOF
+    <!--
+    <php>
+        <server name="KERNEL_DIR" value="/path/to/your/app/" />
+    </php>
+    -->
+EOF;
+        $phpunitKernelAfter = <<<EOF
+    <php>
+        <server name="KERNEL_DIR" value="$appDir/" />
+    </php>
+EOF;
+        $phpunit = str_replace(array('<directory>../src/', '"bootstrap.php.cache"', $phpunitKernelBefore), array('<directory>src/', '"'.$varDir.'/bootstrap.php.cache"', $phpunitKernelAfter),  file_get_contents($rootDir.'/phpunit.xml.dist'));
         $composer = str_replace("\"symfony-app-dir\": \"app\",", "\"symfony-app-dir\": \"app\",\n        \"symfony-bin-dir\": \"bin\",\n        \"symfony-var-dir\": \"var\",", file_get_contents($rootDir.'/composer.json'));
         $travis = str_replace("\nscript: phpunit -c app", '', file_get_contents($rootDir.'/.travis.yml'));
 
