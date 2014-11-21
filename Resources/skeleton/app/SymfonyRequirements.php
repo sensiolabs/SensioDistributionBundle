283b2a8fd6acbac2cec4d9a84203224d9716a6e3
Javier Eguiluz <javier.eguiluz@gmail.com>
2014-11-21 14:44:01 +0100

    Added a recommendation about mbstring.func_overload option

--- Resources/skeleton/app/SymfonyRequirements.php
+++ Resources/skeleton/app/SymfonyRequirements.php
@@ -689,6 +689,14 @@ class SymfonyRequirements extends RequirementCollection
 
         $this->addPhpIniRecommendation('session.auto_start', false);
 
+        $this->addPhpIniRecommendation(
+            'mbstring.func_overload',
+            create_function('$cfgValue', 'return (int) $cfgValue === 2;'),
+            true,
+            'string functions should not be overloaded',
+            'Set "<strong>mbstring.func_overload</strong>" to <strong>0</strong> in php.ini<a href="#phpini">*</a> to disable function overloading by the mbstring extension.'
+        );
+
         $this->addRecommendation(
             class_exists('PDO'),
             'PDO should be installed',
