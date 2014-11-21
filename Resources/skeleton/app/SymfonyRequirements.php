05261a3818497f02e29f7f7ddfa2c484c136e528
Javier Eguiluz <javier.eguiluz@gmail.com>
2014-11-21 14:59:25 +0100

    Require a good mbstring.func_overload value only if mbstring is loaded

--- Resources/skeleton/app/SymfonyRequirements.php
+++ Resources/skeleton/app/SymfonyRequirements.php
@@ -530,13 +530,15 @@ class SymfonyRequirements extends RequirementCollection
             'Install the <strong>PCRE</strong> extension (version 8.0+).'
         );
 
-        $this->addPhpIniRequirement(
-            'mbstring.func_overload',
-            create_function('$cfgValue', 'return (int) $cfgValue === 2;'),
-            true,
-            'string functions should not be overloaded',
-            'Set "<strong>mbstring.func_overload</strong>" to <strong>0</strong> in php.ini<a href="#phpini">*</a> to disable function overloading by the mbstring extension.'
-        );
+        if (extension_loaded('mbstring')) {
+            $this->addPhpIniRequirement(
+                'mbstring.func_overload',
+                create_function('$cfgValue', 'return (int) $cfgValue === 2;'),
+                true,
+                'string functions should not be overloaded',
+                'Set "<strong>mbstring.func_overload</strong>" to <strong>0</strong> in php.ini<a href="#phpini">*</a> to disable function overloading by the mbstring extension.'
+            );
+        }
 
         /* optional recommendations follow */
 
