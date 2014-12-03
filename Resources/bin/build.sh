ddcfeaeb504ece5de546dbefd5c527eed7453048
Fabien Potencier <fabien.potencier@gmail.com>
2014-12-03 14:50:45 +0100

    fixed build script for Swiftmailer (using PSR-4 now)

--- Resources/bin/build.sh
+++ Resources/bin/build.sh
@@ -78,7 +78,11 @@ cd $TARGET/swiftmailer/swiftmailer && rm -rf CHANGES README* build* docs notes t
 # Symfony
 cd $TARGET/symfony/symfony && rm -rf README.md phpunit.xml* tests *.sh vendor
 cd $TARGET/symfony/assetic-bundle/Symfony/Bundle/AsseticBundle && rm -rf Tests Resources/doc
-cd $TARGET/symfony/swiftmailer-bundle/Symfony/Bundle/SwiftmailerBundle && rm -rf Tests Resources/doc
+if [ -d $TARGET/symfony/swiftmailer-bundle/Symfony/Bundle/SwiftmailerBundle ]; then
+    cd $TARGET/symfony/swiftmailer-bundle/Symfony/Bundle/SwiftmailerBundle && rm -rf Tests Resources/doc
+else
+    cd $TARGET/symfony/swiftmailer-bundle && rm -rf Tests Resources/doc
+fi
 if [ -d $TARGET/symfony/monolog-bundle/Symfony/Bundle/MonologBundle ]; then
     cd $TARGET/symfony/monolog-bundle/Symfony/Bundle/MonologBundle && rm -rf Tests Resources/doc
 else
